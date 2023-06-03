2ae370e00e91ac43f15e74c415bc589637ecca3d
Gangarig <112808191+Gangarig@users.noreply.github.com>
2023-06-03 23:20:05 +0200

    adjusting components to main branch

--- whaiky/src/components/common/sidebar/Sidebar.jsx
+++ whaiky/src/components/common/sidebar/Sidebar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { AiFillHome } from 'react-icons/ai';
-
+import { AiFillHome, AiFillProfile, AiOutlineMessage } from 'react-icons/ai';
+import { FiSettings } from 'react-icons/fi';
+import { BsWallet } from 'react-icons/bs';
+import { BsPersonCircle } from 'react-icons/bs';
+import user from '../../../../src/assets/images/user.png'
 export default function Sidebar() {
   return (
     <div className='sideBar'>
-      <AiFillHome size={40}/>
+      <img src={user} alt="user"/>
+      <AiFillHome size={40} className='reactIcon'/>
+      <AiFillProfile size={40} className='reactIcon'/>
+      <AiOutlineMessage size={40} className='reactIcon'/>
+      <BsWallet size={40} className='reactIcon'/>
+      <BsPersonCircle size={40} className='reactIcon'/>
+      <FiSettings size={40} className='reactIcon'/>
       <h2>SideBar</h2>
     </div>
   )
